46d1b9e161ba14ad53f6f314ce505c09db23a532
lucio-crDEV <lucio.m.cardenas@gmail.com>
2022-07-24 05:58:12 -0400

    cargando logo delantero y trasero

--- src/app/card/tarjeta/tarjeta.component.ts
+++ src/app/card/tarjeta/tarjeta.component.ts
@@ -1,5 +1,6 @@
-import { Component,} from '@angular/core';
+import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-tarjeta',
@@ -9,8 +10,11 @@ import { NgForm } from '@angular/forms';
 
 export class TarjetaComponent {
 
+  debouncer: Subject<string> = new Subject();
+
   mesesYear = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
-  years     = [22, 23, 24, 25, 26, 27, 28]
+
+  years = [22, 23, 24, 25, 26, 27, 28]
 
   cardNumber    : string              = ''
   cardName      : string              = ''
@@ -26,6 +30,7 @@ export class TarjetaComponent {
   // Agregando clase que rota la tarjeta
   rotate(tarjeta: HTMLElement) {
     tarjeta.classList.toggle('active')
+    
   }
 
   // Mostrar formulario con boton giratorio
@@ -35,15 +40,23 @@ export class TarjetaComponent {
   }
 
   complete( form: NgForm ) {
-    // const inputNumero = document.getElementById('inputNumero')
 
-    // agregar espacio en el input cada 4 
-    // form.value.cardNumber.replace(/([0-9]{4})/g, '$1 ')
+    form.value.cardNumber = this.cardNumber
+      // Eliminando espacios en blanco
+      .replace(/\s/g, '')
+      // Elimina las letras
+      .replace(/\D/g, '')
+      // Agregar espacio cada cuatro números
+      .replace(/([0-9]{4})/g, '$1 ')
+      // Eliminando últimos espaciados
+      .trim()
+    
 
-
-    document.getElementById('numero')?.textContent!=''
+    // document.getElementById('numero')?.textContent!=''
 
     // Desarrollando para cambiar imagen dinamicamente segun codigo verificador
+
+    // Imagen Front
 
      switch (form.value.cardNumber.slice(0, 1)) {
 
@@ -88,8 +101,51 @@ export class TarjetaComponent {
         document.getElementById('logo-marca-trasera')!.innerHTML = ''
     }
 
+    // Imagen Back
+
+    switch (form.value.cardNumber.slice(0, 1)) {
+
+      case "3":
+        document.getElementById('logo-marca-trasera')!.innerHTML = ''
+        let imagen2 = document.createElement('img')
+        imagen2.setAttribute('width','150rem')
+        imagen2.src = '/assets/img/logos/americanexpress.png'
+        document.getElementById('logo-marca-trasera')!.appendChild(imagen2)
+        break;
+
+      case "4":
+        document.getElementById('logo-marca-trasera')!.innerHTML = ''
+        let imagen = document.createElement('img')
+        imagen.setAttribute('width','140rem')
+        imagen.src = '/assets/img/logos/visa.png'
+        document.getElementById('logo-marca-trasera')!.appendChild(imagen)
+        break;
+
+      case "5":
+        document.getElementById('logo-marca-trasera')!.innerHTML = ''
+        let imagen1 = document.createElement('img')
+        imagen1.setAttribute('width','130rem')
+        imagen1.src = '/assets/img/logos/mastercard.png'
+        document.getElementById('logo-marca-trasera')!.appendChild(imagen1)
+        break;
+
+      case "6":
+        document.getElementById('logo-marca-trasera')!.innerHTML = ''
+        let imagen3 = document.createElement('img')
+        imagen3.setAttribute('width','180rem')
+        imagen3.src = '/assets/img/logos/discover.png'
+        document.getElementById('logo-marca-trasera')!.appendChild(imagen3)
+        break;
+
+      case "": 
+        document.getElementById('logo-marca')!.innerHTML = ''
+        document.getElementById('logo-marca-trasera')!.innerHTML = ''
+    }
+
     this.voltearTarjeta()
   }
+
+  // crear una funcion en vez del 2do switch y la variable de entrada sea logo marca trasera y logo marca y luego llamar ambas funciones
 
   voltearTarjeta(){
     if( document.getElementById('tarjeta')?.classList.contains('active') ){
@@ -102,6 +158,19 @@ export class TarjetaComponent {
       document.getElementById('tarjeta')?.classList.add('active')
     }
   }
+
+/* 
+  teclaPresionada(){
+    this.debouncer
+      .next( this.cardNumber)
+      .subscribe( valor => {
+      
+    })
+    
+  } */
+
+
+
 }
 
 
